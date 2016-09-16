66cd9364e3fc9459fa667d7c33b6722faf6019b5
ealtheseal1 <ealund85@gmail.com>
2016-09-16 19:12:30 +0200

    Add files via upload

--- Ranking.m
+++ Ranking.m
@@ -1,8 +1,8 @@
 function [x, loop] = Ranking(A) 
 
 n = size(A,2); %gives size 5
-x = ones(n,1)*1/n; %5x1 matrice filled with 1/5
-x0 = ones(1,n);
+x = ones(n,1)'*1/n; %5x1 matrice filled with 1/5
+x0 = zeros(1,n);
 m = 0.15;  %damping 15 percent
 S = ones(n,n)*1/n;
 epsilon = 0.000000001;
@@ -10,14 +10,14 @@ loop = 1;
 
 while loop<10000
     x0 = x;
-    x = ((1-m)*A + m*S)*x0;
+    x = x0*((1-m)*A + m*S);
     
     if any(((1-m)*A + m*S)^2<epsilon)
         print = ('Matrix is not regular');
         disp(print)
     end
     
-    if (x ~= ((1-m)*A + m*S)*x)
+    if (x ~= x0*((1-m)*A + m*S))
         print2 = ('Matrix is not stochastic');
         disp(print2)
     end
